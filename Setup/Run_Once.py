a40459faa9a201dc5e0c6bdc6972ca8077d27f88
Elias Perez <37880438+Elionidas@users.noreply.github.com>
2018-05-03 11:36:42 -0500

    testedRun_once
    Run_once properly tested

--- Setup/Run_Once.py
+++ Setup/Run_Once.py
@@ -1,45 +1,48 @@
-#!/bin/python
-
 import rethinkdb as r
 import time as T
 
-try: 
+if __name__ == "__main__": 
 
-	r.connect("192.168.43.81", 28015).repl()
-except Exception as Ex:
-	print(Ex)
+	try:
+		r.connect("192.168.43.81", 28015).repl()
+	except Exception as Ex:
+		print(Ex)
 
-try:
-	r.db_create("Plugins").run()
-	
-except Exception as Ex:
-	print (Ex)
-	
-try:
-	r.db_create("Brain").run()
-	r.db("Brain").table_create("Targets").run()
-	r.db("Brain").table_create("Outputs").run()
-except Exception as Ex:
-	print (Ex)
+	try:
+		r.db_create("Plugins").run()
+		
+	except Exception as Ex:
+		print (Ex)
+		
+	try:
+		r.db_create("Brain").run()
+		r.db("Brain").table_create("Targets").run()
+		r.db("Brain").table_create("Outputs").run()
+	except Exception as Ex:
+		print (Ex)
 
-try:
-	r.db("Brain").table_create("Jobs").run()
-except Exception as Ex:
-	print (Ex)
+	try:
+		r.db("Brain").table_create("Jobs").run()
+	except Exception as Ex:
+		print (Ex)
 
-try:
-	r.db("Brain").table("Targets").insert({"Plugin_Name": "Dummy", "Location": "1.1.1.1", "Port": "12345"}).run()
-except Exception as Ex:
-	print (Ex)
-	
-try:
-	r.db("Brain").table("Jobs").insert({"Jobs": "Job_Target", "Status": "pending", "Start_Time": int(T.time()), "Job_Command": "Keylogger"}).run()
-except Exception as Ex:
-	print (Ex)
-	
-try:
-	r.db("Brain").table("Outputs").insert({"Job_Entry": "Job_One", "Content": "StringContent"}).run()
-except Exception as Ex:
-	print (Ex)
-	
-print("complete")
+	try:
+		r.db("Brain").table("Targets").insert({"Plugin_Name": "Dummy", "Location": "1.1.1.1", "Port": "12345"}).run()
+	except Exception as Ex:
+		print (Ex)
+		
+	try:
+		r.db("Brain").table("Jobs").insert({"Jobs": "Job_Target", "Status": "pending", "Start_Time": int(T.time()), "Job_Command": "Keylogger"}).run()
+	except Exception as Ex:
+		print (Ex)
+		
+	try:
+		r.db("Brain").table("Outputs").insert({"Job_Entry": "Job_One", "Content": "StringContent"}).run()
+	except Exception as Ex:
+		print (Ex)
+		
+	test_A()	
+	print("complete")
+def test_A():
+	assert(1==1)
+
