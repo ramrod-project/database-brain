9102b8f1420830c301481e577d3e34d3b667a866
Elias Perez <37880438+Elionidas@users.noreply.github.com>
2018-05-03 10:24:15 -0500

    Run_Once
    Setup for python rethink DB

---
+++ Setup/Run_Once.py
@@ -0,0 +1,47 @@
+#!/bin/python
+
+import rethinkdb as r
+import time as T
+
+try: 
+
+	r.connect("192.168.43.81", 28015).repl()
+except Exception as Ex:
+	print(Ex)
+
+try:
+	r.db_create("Plugins").run()
+	r.db("Plugins").table_create("plugin_A").run()
+	
+except Exception as Ex:
+	print (Ex)
+	
+try:
+	r.db_create("Brain").run()
+	r.db("Brain").table_create("Targets").run()
+	r.db("Brain").table_create("OutPut").run()
+except Exception as Ex:
+	print (Ex)
+
+try:
+	r.db("Brain").table_create("Jobs").run()
+except Exception as Ex:
+	print (Ex)
+
+try:
+	r.db("Brain").table("Targets").insert({"Plugin_Name": "Dummy", "Location": "1.1.1.1", "Port": "12345"}).run()
+except Exception as Ex:
+	print (Ex)
+	
+try:
+	r.db("Brain").table("Jobs").insert({"Jobs": "Job_Target", "Status": "pending", "Start_Time": int(T.time()), "Job_Command": "Keylogger"}).run()
+except Exception as Ex:
+	print (Ex)
+	
+try:
+	r.db("Brain").table("Jobs").insert({"Job_Entry": "Job_One", "Content": "StringContent"}).run()
+except Exception as Ex:
+	print (Ex)
+	
+print("complete")
+	
