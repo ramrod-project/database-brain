9698ebf97ceded6285922ec2cec29c98365ebf80
Elias Perez <37880438+Elionidas@users.noreply.github.com>
2018-05-21 15:12:08 -0500

    added _list().contains

--- setup/test_integration.py
+++ setup/test_integration.py
@@ -25,30 +25,23 @@ def rethink():
     except SystemExit:
         pass
 
-def test_brain(rethink):
-    r.connect("127.0.0.1", 28015)
-    r.db('Brain').run(rethink)
+def test_brain(rethink):  
+    r.db_list().contains('Brain').run(r.connect("127.0.0.1", 28015))
 
-def test_plugins(rethink):
-    r.connect("127.0.0.1", 28015)
-    r.db('Plugins').run(rethink)
+def test_plugins(rethink):  
+    r.db_list().contains('Plugins').run(r.connect("127.0.0.1", 28015))
 
 def test_brain_targets(rethink):
-    r.connect("127.0.0.1", 28015)
-    r.db("Brain").table('Targets').run(rethink)
+    r.db("Brain").table('Targets').run(r.connect("127.0.0.1", 28015))
 
-def test_brain_output(rethink):
-    r.connect("127.0.0.1", 28015)
-    r.db("Brain").table('Outputs').run(rethink)
+def test_brain_output(rethink):  
+    r.db("Brain").table('Outputs').run(r.connect("127.0.0.1", 28015))
 
-def test_brain_jobs(rethink):
-    r.connect("127.0.0.1", 28015)
-    r.db("Brain").table('Jobs').run(rethink)
+def test_brain_jobs(rethink):   
+    r.db("Brain").table('Jobs').run(r.connect("127.0.0.1", 28015))
 
 def test_audit(rethink):
-    r.connect("127.0.0.1", 28015)
-    r.db('Audit').run(rethink)
+    r.db('Audit').run(r.connect("127.0.0.1", 28015))
 
-def test_audit_jobs(rethink):
-    r.connect("127.0.0.1", 28015)
-    r.db("Audit").table('Jobs').run(rethink)
+def test_audit_jobs(rethink):    
+    r.db("Audit").table('Jobs').run(r.connect("127.0.0.1", 28015))
