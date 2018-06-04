3e39974f9bc091914df94e87badc37de8df7cbda
Dan Bauman <dan@bauman.space>
2018-06-04 17:32:33 -0500

    derp

--- schema/test_environment_check.py
+++ schema/test_environment_check.py
@@ -6,49 +6,70 @@ def test_default_loglevel():
     if "LOGLEVEL" in environ:
         del(environ['LOGLEVEL'])
     assert check_log_env() == "TEST"
+    assert not check_prod_env()
+    assert check_dev_env()
 
 def test_emergency_loglevel():
     environ['LOGLEVEL'] = "EMERGENCY"
     assert log_env_gte("EMERGENCY")
     assert not log_env_gte("TEST")
+    assert not check_prod_env()
+    assert check_dev_env()
 
 def test_warning_loglevel():
     environ['LOGLEVEL'] = "WARNING"
     assert log_env_gte("WARNING")
     assert not log_env_gte("TEST")
+    assert not check_prod_env()
+    assert check_dev_env()
 
 def test_debug_loglevel():
     environ['LOGLEVEL'] = "DEBUG"
     assert log_env_gte("WARNING")
     assert log_env_gte("DEBUG")
     assert not log_env_gte("TEST")
+    assert not check_prod_env()
+    assert check_dev_env()
 
 def test_test_loglevel():
     environ['LOGLEVEL'] = "TEST"
     assert log_env_gte("WARNING")
     assert log_env_gte("DEBUG")
     assert log_env_gte("TEST")
+    assert not check_prod_env()
+    assert check_dev_env()
 
 def test_stage_env():
     if "STAGE" in environ:
         del(environ['STAGE'])
     assert check_stage_env() == "TESTING"
+    assert not check_prod_env()
+    assert check_dev_env()
 
 def test_dev_env():
     if "STAGE" in environ:
         del (environ['STAGE'])
     assert check_dev_env()
+    assert not check_prod_env()
 
 
 def test_not_dev_env():
     if "STAGE" in environ:
         del (environ['STAGE'])
     assert not check_prod_env()
+    assert check_dev_env()
 
 def test_set_bad_stage():
     environ['STAGE'] = "PRODUCTION"
     assert not check_prod_env()
+    assert check_dev_env()
 
-def test_set_bad_stage():
+def test_set_dev_check_stage():
     environ['STAGE'] = "DEV"
     assert check_dev_env()
+    assert not check_prod_env()
+
+def test_set_prod__check_stage():
+    environ['STAGE'] = "PROD"
+    assert not check_dev_env()
+    assert check_prod_env()
