818f3649a0853ba8d6528c838248698d84a04ccc
Dan Bauman <dan@bauman.space>
2018-07-23 13:49:48 -0500

    fixed objects may evaluate to true if empty

--- schema/test_schema_job.py
+++ schema/test_schema_job.py
@@ -1,5 +1,6 @@
 from .brain.checks import verify, strip
 from .brain import brain_pb2 as b
+from .brain.jobs import verify_job, verify_jobs
 from .test_schema_target import Good_TARGET
 from .test_schema_command import GoodCommand
 
@@ -58,26 +59,62 @@ Badjob7 = {"id": "string",
            "JobCommand": GoodCommand,
            "testcase": True}
 
+
 def test_good_job():
     assert verify(Goodjob, b.Job())
+
 
 def test_bad_job():
     assert not verify(Badjob, b.Job())
 
+
 def test_bad_job2():
     assert not verify(Badjob2, b.Job())
+
 
 def test_bad_job3():
     assert not verify(Badjob3, b.Job())
 
+
 def test_bad_job4():
     assert not verify(Badjob4, b.Job())
+
 
 def test_bad_job5():
     assert not verify(Badjob5, b.Job())
 
+
 def test_bad_job6():
     assert not verify(Badjob6, b.Job())
 
+
 def test_bad_job7():
     assert not verify(Badjob7, b.Job())
+
+
+def test_good_job_from_jobs():
+    assert verify_job(Goodjob)
+
+
+def test_good_jobs_from_jobs():
+    assert verify_jobs([Goodjob])
+
+
+def test_not_a_job():
+    assert not verify_job(None)
+
+
+def test_not_a_jobs():
+    assert not verify_jobs(None)
+
+
+def test_list_bad_jobs():
+    assert not verify_jobs([Goodjob, Badjob2])
+
+
+def test_list_good_jobs():
+    assert verify_jobs([Goodjob, Goodjob, Goodjob])
+
+
+def test_list_some_good_jobs():
+    assert not verify_jobs([Goodjob, Goodjob, Badjob6])
