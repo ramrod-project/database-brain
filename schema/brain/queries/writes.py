67ffe5658429f8efc3cff0cb91fa9ed162472a7a
Dan Bauman <dan@bauman.space>
2018-08-01 12:24:24 -0500

    apply warning to insert_new_target

--- schema/brain/queries/writes.py
+++ schema/brain/queries/writes.py
@@ -33,6 +33,7 @@ def _check_port_conflict(port_data,
     return has_port_conflict(port_data, existing)
 
 
+@deprecated_function(replacement="brain.queries.write.insert_target")
 @wrap_rethink_errors
 @wrap_connection
 def insert_new_target(plugin_name, location_num,
