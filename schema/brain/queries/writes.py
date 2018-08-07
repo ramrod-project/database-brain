d4018459a71db682dea2069bb836c7d44926b767
Dan Bauman <dan@bauman.space>
2018-08-07 10:10:51 -0500

    pcp-512 unique names only

--- schema/brain/queries/writes.py
+++ schema/brain/queries/writes.py
@@ -98,9 +98,9 @@ def update_job_status(job_id, status, conn=None):
     """
     if status not in VALID_STATES:
         raise ValueError("Invalid status")
-    job_update = RBJ.get(job_id).update({"Status": status}).run(conn)
+    job_update = RBJ.get(job_id).update({STATUS_FIELD: status}).run(conn)
     if job_update["replaced"] == 0 and job_update["unchanged"] == 0:
-        raise ValueError("".join(["Unknown Job", job_id]))
+        raise ValueError("Unknown job_id: {}".format(job_id))
     id_filter = (r.row["OutputJob"]["id"] == job_id)
     output_job_status = {"OutputJob": {"Status": status}}
     output_update = RBO.filter(id_filter).update(output_job_status).run(conn)
